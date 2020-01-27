d0d7b73fb935e9f7962300c6224e712fc4e334f1
Jaime Burbano <burbanojaime98@gmail.com>
2020-01-26 19:14:40 -0500

    Clase 15 Usando React

--- react/src/js/Components/App/App.js
+++ react/src/js/Components/App/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
+import data from '../../../json/data.json'
+import Loader from '../loader/Loader';
 
+console.log(data.loaders);
 const App = () => {
+
+    const [loaderList, setLoaderList] = React.useState([])
+
+    const handleClick = () => {
+      setLoaderList(data.loaders)
+    }
+
     return(
-        <>
-            <p>Que linda aplicación hecha en React</p>
-        </>
-    )
-}
+        <>         
+            <ul>
+                {
+                loaderList.map(item => <Loader data={item.name} key={item.id}/> )
+                }
+            </ul>
+            
+            <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
+         </>
+    )	  
+}	
 
 export default App;
--- react/src/js/index.js
+++ react/src/js/index.js
@@ -3,6 +3,6 @@ import '../css/index.css'
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './Components/App/App';
+import App from './Components/app/App';
 
 render(<App />, document.getElementById('app'));
